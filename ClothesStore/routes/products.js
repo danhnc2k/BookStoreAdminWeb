eb2fc02cad9669d6a42481e3fd9c7847f29ebe83
hieutruong399 <57211229+hieutruong399@users.noreply.github.com>
2020-12-01 17:31:36 +0700

    update DeleteProduct function

--- ClothesStore/routes/products.js
+++ ClothesStore/routes/products.js
@@ -8,8 +8,6 @@ router.get('/:slug/detail', productsController.detail);
 router.get('/create', productsController.create);
 router.post('/store', productsController.store);
 router.delete('/:id', productsController.delete);
-router.get('/:id/edit', productsController.edit);
-router.put('/:id', productsController.update);
 
 
 module.exports = router;
