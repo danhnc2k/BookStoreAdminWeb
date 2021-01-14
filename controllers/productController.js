b6e926ecdeb51f35b3ba0fb75310d1caf220e633
hieutruong399 <hieutruong399@gmail.com>
2021-01-15 02:25:34 +0700

    Chỉnh sửa cho chỉnh chu

--- controllers/productController.js
+++ controllers/productController.js
@@ -16,7 +16,7 @@ exports.getProducts = async function(req, res, next){
         [0, 50],
         [51, 100],
         [101, 150],
-        [151, 200],
+        [151, 200]
     ];
     //Size list
     let sizeList = ["S", "M", "L", "XL"];
@@ -47,12 +47,7 @@ exports.getProducts = async function(req, res, next){
     }
     if (price_value && price_value != -1)
     {
-        if (price_value == priceList.length){
-            filter['price'] = {$gt: priceList[price_value - 1][0]};
-        }
-        else {
-            filter['price'] = {$gt: priceList[price_value][0], $lt: priceList[price_value][1]};
-        }
+        filter['price'] = {$gt: priceList[price_value][0], $lt: priceList[price_value][1]};
     }
     if (size_value && size_value != -1)
     {
@@ -66,7 +61,6 @@ exports.getProducts = async function(req, res, next){
     {
         filter['name'] = new RegExp(`${search_value}`, "i");
     }
-    filter['isDeleted'] = 'false';
     const paginate = await productServices.listProducts(filter, page, itemPerPageList[length_value]);
     res.render("products", {
         categories: categoriesList,
@@ -131,15 +125,10 @@ exports.getAddProductForm = async function (req, res, next) {
     }
     const categoriesList = await categoryServices.listCategories();
     const labelsList = await labelServices.listLabels();
-    var message = undefined;
-    if (req.session.error){
-        message = req.session.error;
-        req.session.error = undefined;
-    }
+
     res.render('addProduct',{
         categories: categoriesList,
-        labels: labelsList,
-        message,
+        labels: labelsList
     });
 }
 exports.getProductDetail = async function(req, res, next){
@@ -195,29 +184,10 @@ exports.addProduct = async function(req, res, next){
         res.redirect("/");
         return;
     }
-    var message = [];
-    if (req.body.name == ""){
-        message.push("Vui lòng không để trống trường Name");
-    }
-    if (req.body.subCategory == -1){
-        message.push("Vui lòng không để trống trường SubCategory");
-    }
-    if (req.body.label == -1){
-        message.push("Vui lòng không để trống trường Label");
-    }
-    if (req.body.image == ""){
-        message.push("Vui lòng không để trống trường Image");
-    }
-    if (message.length != 0){
-        req.session.error = message;
-        res.redirect('back');
-    }
-    else {
-        await productServices.add(req.body.name,req.body.description,req.body.size,
-            req.body.subCategory,req.body.stock,req.body.price,req.body.color,req.body.image,
-            req.body.material,req.body.label,req.body.sale,req.body.cost);
-        res.redirect('/products/list');
-    }
+    await productServices.add(req.body.name,req.body.description,req.body.size,
+        req.body.subCategory,req.body.stock,req.body.price,req.body.color,req.body.image,
+        req.body.material,req.body.label,req.body.sale,req.body.cost);
+    res.redirect('/products/list');
 }
 
 exports.updateProduct = async function(req, res, next){
